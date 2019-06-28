4bedd554d25f01a239e1604f297d9adc92ad5121
MichalAW <michal.wantaeuco@gmail.com>
2019-06-28 23:09:59 +0200

    task finished

--- actions.js
+++ actions.js
@@ -1,18 +1,41 @@
-import {ADD_COMMENT, REMOVE_COMMENT, EDIT_COMMENT} from './actionTypes'
+import {ADD_COMMENT, REMOVE_COMMENT, EDIT_COMMENT, THUMB_UP_COMMENT, THUMB_DOWN_COMMENT} from './actionTypes'
 import uuid from uuid;
 
-const REMOVE_COMMENT = 'REMOVE_COMMENT';
-const EDIT_COMMENT = 'EDIT_COMMENT';
-const THUMB_UP_COMMENT = 'THUMB_UP_COMMENT';
-const THUMB_DOWN_COMMENT = 'THUMB_DOWN_COMMENT';
-
-{
-    type: REMOVE_COMMENT,
-    id: 20 // przykładowy id komentarza do usunięcia
+export function addComment(text) {
+    return {
+        type: ADD_COMMENT,
+        text: text,
+        id: uuid.v4()
+    }
 }
 
-{
-    type: EDIT_COMMENT,
-    id: 20,
-    text: 'wyedytowany tekst komentarza'
+export function removeComment(id) {
+    return {
+        type: REMOVE_COMMENT,
+        id: id
+    }
+}
+
+export function editComment(id, text) {
+    return {
+        type: EDIT_COMMENT,
+        text: text,
+        id: id
+    }
+}
+
+export function thumbUpComment(id, text) {
+    return {
+        type: EDIT_COMMENT,
+        text: text,
+        id: id
+    }
+}
+
+export function thumbDownComment(id, text) {
+    return {
+        type: EDIT_COMMENT,
+        text: text,
+        id: id
+    }
 }
